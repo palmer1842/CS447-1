325d84d7bb810380be816ef162675f0ff25cd23b
Jake Palmer <jake.palmer1842@gmail.com>
2019-10-07 18:29:44 -0700

    Add 'type' property to Tile
    Also add a method to get the type so that the game can check
    whether to allow a Vehicle to move into that tile.

--- src/Tile.java
+++ src/Tile.java
@@ -6,15 +6,22 @@ public class Tile extends Entity {
 	static final int LAND_TYPE = 0;
 	static final int ROAD_TYPE = 1;
 
+	int type;
+
 	Tile(int type, float x, float y) {
 		super(x, y);
 		switch(type) {
 			case LAND_TYPE:
 				addImage(ResourceManager.getImage(CopsAndRobbers.LAND_RSC));
+				this.type = LAND_TYPE;
 				break;
 			case ROAD_TYPE:
+				this.type = ROAD_TYPE;
 				addImage(ResourceManager.getImage(CopsAndRobbers.ROAD_RSC));
 		}
 	}
 
+	int getType() {
+		return type;
+	}
 }
